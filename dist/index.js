972f90307006e5c878c5b99aa16b7261343b2a4d
JHSeo <qnfqnfqnf@gmail.com>
2022-09-10 22:17:29 +0900

    initialize ts-node with esm

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+function cli() {
+    console.log(`${chalk.cyan('Hello')} ${chalk.red('World')}`);
+}
+cli();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+function cli() {
+  console.log(`${chalk.cyan('Hello')} ${chalk.red('World')}`);
+}
+
+cli();
