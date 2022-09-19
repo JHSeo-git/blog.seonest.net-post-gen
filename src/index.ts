658977f913bebf8f160544b19888bade9d0a679e
rusty <namezin@lgcns.com>
2022-09-19 13:52:23 +0900

    feat: prompts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,43 @@
 import chalk from 'chalk';
 import { Command } from 'commander';
-import inquirer from 'inquirer';
 
-import packageJson from '../package.json';
-import questions from './questions.js';
-import type { MDXFrontMatter } from './types.js';
+import type { FrontMatter } from './promts.js';
+import { getPromptThumbnail } from './promts.js';
+import { getPromptCategory, getPromptDescription, getPromptTitle } from './promts.js';
+// import packageJson from '../package.json';
+
+const TARGET_DIR = '__post';
 
 const program = new Command();
 
-program.version(packageJson.version);
-program.name(chalk.cyan(packageJson.name));
+program.version('0.0.1');
+program.name(chalk.cyan('seonest-post-gen'));
+// program.version(packageJson.version);
+// program.name(chalk.cyan(packageJson.name));
 
 program.option('-y', 'initialize using default configuration');
-
 program.parse();
 
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    console.log(answers);
-  })
-  .catch((err) => console.error(err));
+async function gen() {
+  const date = new Date().toISOString();
+
+  const category = await getPromptCategory(TARGET_DIR);
+  const title = await getPromptTitle(TARGET_DIR, category);
+  const description = await getPromptDescription();
+  const thumbnail = await getPromptThumbnail();
+
+  const frontMatter: FrontMatter = {
+    date,
+
+    category,
+    title,
+    description,
+    draft: false,
+    tags: undefined,
+    thumbnail,
+  };
+
+  console.log(frontMatter);
+}
+
+gen();
---
+++ src/promts.ts
@@ -0,0 +1,157 @@
+import path from 'node:path';
+
+import slugify from 'cjk-slug';
+import fs from 'fs-extra';
+import inquirer from 'inquirer';
+
+const __dirname = process.cwd();
+
+// title
+// description
+// date
+// category
+// draft
+// tags
+// thumbnail
+export type FrontMatter = {
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  draft: boolean;
+  tags?: string;
+  thumbnail?: string;
+};
+
+const getSlug = (name: string) => {
+  return slugify(name);
+};
+
+export const getPromptTitle = async (targetDir: string, category: string) => {
+  const { title } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Please input your title',
+      default: () => 'New post title',
+      validate: async (val) => {
+        if (typeof val !== 'string') {
+          return 'Title must be string';
+        }
+
+        if (val.includes("'")) {
+          return 'Cannot use single quote';
+        }
+
+        const slug = getSlug(val);
+        const dest = `${targetDir}/${category}/${slug}.md`;
+        const destFileExists = fs.existsSync(dest);
+
+        if (destFileExists) {
+          return `Already exist file name: ${slug}.md.`;
+        }
+
+        return true;
+      },
+    },
+  ]);
+
+  return title as string;
+};
+
+export const getPromptDescription = async () => {
+  const { description } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please input your description',
+      default: () => 'New post description',
+      validate: async (val) => {
+        if (typeof val !== 'string') {
+          return 'Description must be string';
+        }
+
+        if (val.includes("'")) {
+          return 'Cannot use single quote';
+        }
+        return true;
+      },
+    },
+  ]);
+
+  return description as string;
+};
+
+export const getPromptCategory = async (targetDir: string) => {
+  let category: string;
+  const newCategoryOption = '[CREATE NEW CATEGORY]';
+  const categories = await getCategories(targetDir);
+  const choices = [...categories, new inquirer.Separator(), newCategoryOption];
+
+  const { selectedCategory } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'selectedCategory',
+      message: 'Please select category',
+      choices,
+    },
+  ]);
+
+  if (selectedCategory === newCategoryOption) {
+    const { newCategory } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'newCategory',
+        message: 'Please input new category',
+        validate: (val) => {
+          if (typeof val !== 'string') {
+            return 'Category must be string';
+          }
+
+          if (val.includes("'")) {
+            return 'Cannot use single quote';
+          }
+
+          if (categories.includes(val)) {
+            return `Already exists category: ${val}`;
+          }
+
+          return true;
+        },
+      },
+    ]);
+
+    category = newCategory;
+  } else {
+    category = selectedCategory;
+  }
+
+  return category;
+};
+
+export const getPromptThumbnail = async () => {
+  const { thumbnail } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'thumbnail',
+      message: 'Please input your thumbnail',
+      validate: async (val) => {
+        if (val.includes("'")) {
+          return 'Cannot use single quote';
+        }
+        return true;
+      },
+    },
+  ]);
+
+  return thumbnail as string | undefined;
+};
+
+// TODO: tags
+
+const getCategories = async (targetDir: string) => {
+  const dirPath = path.resolve(__dirname, targetDir);
+  const dirs = fs.readdirSync(dirPath);
+
+  return dirs;
+};
